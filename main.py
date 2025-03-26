1d230fda8e76cb7a40098c6c9a7d7457893f3572
NicoChlebo <nico.chlebo@gmail.com>
2025-03-26 14:48:59 +0100

    wip clean code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from personnage.creer_perso import CreerPersonnage
 from personnage.recap_perso import RecapPerso
 
-# Programme principale 
+# Programme principal 
 creer_personnage = CreerPersonnage.creer_personnage()
 RecapPerso.recap_perso(creer_personnage)
