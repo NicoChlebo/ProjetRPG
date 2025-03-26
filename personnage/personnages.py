e6db10764a8cbda226d3d45ec0837f69bf63da65
NicoChlebo <nico.chlebo@gmail.com>
2025-03-26 16:02:41 +0100

    wip LSP on personnage

--- personnage/personnages.py
+++ personnage/personnages.py
@@ -1,10 +1,6 @@
 from classes.classe import Classe
 
-class Personnage(Classe):
-    def __init__(self, nom_perso, classe_perso):
-        super().__init__(classe_perso.nom, classe_perso.pv, classe_perso.pm, classe_perso.force,
-                         classe_perso.intelligence, classe_perso.défense, 
-                         classe_perso.résistance_magique, classe_perso.agilité, 
-                         classe_perso.chance, classe_perso.endurance, classe_perso.esprit)
+class Personnage:
+    def __init__(self, nom_perso: str, classe_perso: Classe):
         self.nom_personnage = nom_perso
         self.classe = classe_perso
